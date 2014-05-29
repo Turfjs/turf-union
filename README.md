20913d453ae56795fc765dd27344c287186fdac3
Tom MacWright <tom@macwright.org>
2014-05-29 14:37:55 -0400

    First

---
+++ README.md
@@ -0,0 +1,5 @@
+[![build status](https://secure.travis-ci.org/Turfjs/turf-union.png)](http://travis-ci.org/Turfjs/turf-union)
+
+# turf-union
+
+Find the union of geographic features.
---
+++ index.js
@@ -0,0 +1,21 @@
+// look here for help http://svn.osgeo.org/grass/grass/branches/releasebranch_6_4/vector/v.overlay/main.c
+//must be array of polygons
+
+// depend on jsts for now https://github.com/bjornharrtell/jsts/blob/master/examples/overlay.html
+
+var jsts = require('jsts')
+
+module.exports = function(poly1, poly2){
+  var reader = new jsts.io.GeoJSONReader()
+  var a = reader.read(JSON.stringify(poly1.geometry))
+  var b = reader.read(JSON.stringify(poly2.geometry))
+  var union = a.union(b);
+  var parser = new jsts.io.GeoJSONParser()
+
+  union = parser.write(union)
+  return {
+    type: 'Feature',
+    geometry: union,
+    properties: poly1.properties
+  }
+}
