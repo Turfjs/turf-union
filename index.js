706bfcf7f573d856d48e14e1d55f3035ba1af2cc
Tim Channell <tim.channell@gmail.com>
2015-02-11 20:12:59 -0500

    removed jsts

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// look here for help http://svn.osgeo.org/grass/grass/branches/releasebranch_6_4/vector/v.overlay/main.c
-//must be array of polygons
-
-// depend on jsts for now https://github.com/bjornharrtell/jsts/blob/master/examples/overlay.html
-
-var jsts = require('jsts');
 var gh = require('greiner-hormann');
 
 /**
